70eba7e34d21a926b295bef5b5433cf84d7cd9f3
KarazhanSucks <klotedolfijn@memeware.net>
2022-07-28 14:28:29 +0200

    Taragaman Untested First Pass

--- DBM-Party-Classic/RagefireChasm/Taragaman.lua
+++ DBM-Party-Classic/RagefireChasm/Taragaman.lua
@@ -2,17 +2,18 @@ local mod	= DBM:NewMod("Taragaman", "DBM-Party-Classic", 9)
 local L		= mod:GetLocalizedStrings()
 mod.statTypes = "normal,heroic,mythic"
 
-mod:SetRevision("20220712224943KSA")
+mod:SetRevision("20220725002300KSM")
 mod:SetCreatureID(11520)
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_SUCCESS 18072 11970"
+	"SPELL_CAST_SUCCESS 18072 11970",
+	"SPELL_CAST_START 11970"
 )
 
 local warningUppercut			= mod:NewSpellAnnounce(18072, 3, nil, "Tank", 2)
-local warningFireNova			= mod:NewSpellAnnounce(11970, 3)
+local warningFireNova			= mod:NewCastAnnounce(11970, 3, 2)
 
 local timerUppercutCD			= mod:NewAITimer(180, 18072, nil, "Tank", 2, 5, nil, DBM_COMMON_L.TANK_ICON)
 local timerFireNovaCD			= mod:NewAITimer(180, 11970, nil, nil, nil, 2, nil, DBM_COMMON_L.HEALER_ICON)
@@ -26,8 +27,13 @@ function mod:SPELL_CAST_SUCCESS(args)
 	if args.spellId == 18072 then
 		warningUppercut:Show()
 		timerUppercutCD:Start()
-	elseif args.spellId == 11970 then
+	end
+end
+
+function mod:SPELL_CAST_START(args)
+	if args.spellId == 11970 then
 		warningFireNova:Show()
+		warningFireNova:Play("aesoon")
 		timerFireNovaCD:Start()
 	end
 end
