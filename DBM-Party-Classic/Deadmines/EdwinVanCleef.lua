417f933ad5be444e6168dbb06ccf0cf7b97565e9
KarazhanSucks <klotedolfijn@memeware.net>
2022-07-28 14:14:25 +0200

    Edwin Untested Second Pass

--- DBM-Party-Classic/Deadmines/EdwinVanCleef.lua
+++ DBM-Party-Classic/Deadmines/EdwinVanCleef.lua
@@ -8,7 +8,7 @@ mod:SetCreatureID(639)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_SUCCESS 5200 3391",
+	"SPELL_CAST_SUCCESS 5200 3391"
 )
 
 local warningThrash					= mod:NewSpellAnnounce(3391, 3)
@@ -17,7 +17,7 @@ local warningAllies					= mod:NewSpellAnnounce(5200, 3)
 local timerTrashCD					= mod:NewAITimer(180, 3391, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 
 function mod:OnCombatStart(delay)
-	timerTrashD:Start(1-delay)
+	timerTrashCD:Start(1-delay)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
