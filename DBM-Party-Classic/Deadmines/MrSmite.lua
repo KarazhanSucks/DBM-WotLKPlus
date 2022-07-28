1f0e9a940235c44884e85a4b5cf14081c41c1938
KarazhanSucks <klotedolfijn@memeware.net>
2022-07-28 14:37:03 +0200

    MrSmite Untested First Pass

--- DBM-Party-Classic/Deadmines/MrSmite.lua
+++ DBM-Party-Classic/Deadmines/MrSmite.lua
@@ -8,7 +8,7 @@ mod:SetCreatureID(646)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_SUCCESS 6432",
+	"SPELL_CAST_SUCCESS 6432 3391 6435",
 	"SPELL_AURA_APPLIED 6435 6264"
 )
 
@@ -16,10 +16,18 @@ local warningSmiteSlam		= mod:NewTargetNoFilterAnnounce(6435, 2)
 local warningNimbleReflexes	= mod:NewTargetNoFilterAnnounce(6264, 2)
 
 local timerSmiteStomp		= mod:NewBuffFadesTimer(10, 6432, nil, nil, nil, 2)
+local timerSmiteSlam		= mod:NewBuffFadesTimer(10, 6435, nil, nil, nil, 2)
+local timerTrashCD			= mod:NewAITimer(180, 3391, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args.spellId == 6432 then
 		timerSmiteStomp:Start()
+	elseif args.spellId == 3391 and args:IsDestTypeHostile() then
+		warningThrash:Show()
+		timerTrashCD:Start()
+	elseif args.spellId == 6435 and args:IsDestTypeHostile() then
+		warningSmiteSlam:Show()
+		timerSmiteSlam:Start()
 	end
 end
 
