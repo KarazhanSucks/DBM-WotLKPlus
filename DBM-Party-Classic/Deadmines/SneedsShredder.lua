baa3876d9c31e850fb32e890a34f732e0fd7d60a
KarazhanSucks <klotedolfijn@memeware.net>
2022-07-28 14:43:06 +0200

    Sneed Untested First Pass

--- DBM-Party-Classic/Deadmines/SneedsShredder.lua
+++ DBM-Party-Classic/Deadmines/SneedsShredder.lua
@@ -2,18 +2,20 @@ local mod	= DBM:NewMod("SneedsShredder", "DBM-Party-Classic", 5)
 local L		= mod:GetLocalizedStrings()
 mod.statTypes = "normal,heroic,mythic"
 
-mod:SetRevision("20220712224943KSA")
+mod:SetRevision("20220728144200")
 mod:SetCreatureID(642, 643)--Shredder, Sneed
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_SUCCESS 7399 6713 5141",
+	"SPELL_CAST_START 7399 6713 5141";
 	"SPELL_AURA_APPLIED 7399 6713"
 )
 
 local warningFear			= mod:NewTargetNoFilterAnnounce(7399, 2)
 local warningDisarm			= mod:NewTargetNoFilterAnnounce(6713, 2)
+local warningDistractingPain= mod:NewTargetNoFilterAnnounce(3603, 2)
 local warningEjectSneed		= mod:NewSpellAnnounce(5141, 2)
 
 local timerFearCD			= mod:NewAITimer(180, 7399, nil, nil, nil, 3, nil, DBM_COMMON_L.MAGIC_ICON)
@@ -35,10 +37,24 @@ function mod:SPELL_CAST_SUCCESS(args)
 	end
 end
 
+function mod:SPELL_CAST_START(args)
+	if args.spellId == 7399 and args:IsSrcTypeHostile() then
+		timerFearCD:Start()
+	elseif args.spellId == 6713 and args:IsSrcTypeHostile() then
+		timerDisarmCD:Start()
+	elseif args.spellId == 5141 then
+		warningEjectSneed:Show()
+		timerFearCD:Stop()
+		timerDisarmCD:Start(1)
+	end
+end
+
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 7399 and args:IsDestTypePlayer() then
 		warningFear:Show(args.destName)
 	elseif args.spellId == 6713 and args:IsDestTypePlayer() then
 		warningDisarm:Show(args.destName)
+	elseif args.spellId == 3603 and args:IsDestTypePlayer() then
+		warningDistractingPain:Show(args.destName)
 	end
 end
