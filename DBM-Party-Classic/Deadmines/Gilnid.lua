27d78449575ceef84a7d4d1a9a85798fd87178b0
KarazhanSucks <klotedolfijn@memeware.net>
2022-07-28 14:16:34 +0200

    Gilnid Untested First Pass

--- DBM-Party-Classic/Deadmines/Gilnid.lua
+++ DBM-Party-Classic/Deadmines/Gilnid.lua
@@ -2,14 +2,14 @@ local mod	= DBM:NewMod("Gilnid", "DBM-Party-Classic", 5)
 local L		= mod:GetLocalizedStrings()
 mod.statTypes = "normal,heroic,mythic"
 
-mod:SetRevision("20220712224943KSA")
+mod:SetRevision("20220728151500")
 mod:SetCreatureID(1763)
 mod:SetModelID(622) -- temporary model, to prevent HD client crash on model preview
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_SUCCESS 5213",
+	"SPELL_CAST_START 5213",
 	"SPELL_AURA_APPLIED 5213"
 )
 
@@ -21,7 +21,7 @@ function mod:OnCombatStart(delay)
 	timerMoltenMetalCD:Start(1-delay)
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
+function mod:SPELL_CAST_START(args)
 	if args.spellId == 5213 then
 		timerMoltenMetalCD:Start()
 	end
