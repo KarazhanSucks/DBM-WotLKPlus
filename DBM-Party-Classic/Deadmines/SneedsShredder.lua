1cd480ffe8ce47068340fd7a075608cc0beba933
KarazhanSucks <klotedolfijn@memeware.net>
2022-07-28 14:48:46 +0200

    Sneed Untested Second Pass

--- DBM-Party-Classic/Deadmines/SneedsShredder.lua
+++ DBM-Party-Classic/Deadmines/SneedsShredder.lua
@@ -8,9 +8,9 @@ mod:SetCreatureID(642, 643)--Shredder, Sneed
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_SUCCESS 7399 6713 5141",
-	"SPELL_CAST_START 7399 6713 5141";
-	"SPELL_AURA_APPLIED 7399 6713"
+	"SPELL_CAST_SUCCESS 7399 6713 5141 3603",
+	"SPELL_CAST_START 7399 6713 5141 3603";
+	"SPELL_AURA_APPLIED 7399 6713 3603"
 )
 
 local warningFear			= mod:NewTargetNoFilterAnnounce(7399, 2)
@@ -18,8 +18,9 @@ local warningDisarm			= mod:NewTargetNoFilterAnnounce(6713, 2)
 local warningDistractingPain= mod:NewTargetNoFilterAnnounce(3603, 2)
 local warningEjectSneed		= mod:NewSpellAnnounce(5141, 2)
 
-local timerFearCD			= mod:NewAITimer(180, 7399, nil, nil, nil, 3, nil, DBM_COMMON_L.MAGIC_ICON)
-local timerDisarmCD			= mod:NewAITimer(180, 6713, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerFearCD				= mod:NewAITimer(180, 7399, nil, nil, nil, 3, nil, DBM_COMMON_L.MAGIC_ICON)
+local timerDistractingPain	= mod:NewAITimer(180, 3603, nil, nil, nil, 3, nil, DBM_COMMON_L.MAGIC_ICON)
+local timerDisarmCD				= mod:NewAITimer(180, 6713, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 
 function mod:OnCombatStart(delay)
 	timerFearCD:Start(1-delay)
@@ -46,6 +47,8 @@ function mod:SPELL_CAST_START(args)
 		warningEjectSneed:Show()
 		timerFearCD:Stop()
 		timerDisarmCD:Start(1)
+	elseif args.spellId == 3603 then
+		timerDistractingPain:Start()
 	end
 end
 
