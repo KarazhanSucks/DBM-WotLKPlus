83e4aa2bc7d48b34a5e12fe27d1afb9fb7a9009f
KarazhanSucks <klotedolfijn@memeware.net>
2022-07-28 14:29:16 +0200

    Taragaman Untested Second Pass

--- DBM-Party-Classic/RagefireChasm/Taragaman.lua
+++ DBM-Party-Classic/RagefireChasm/Taragaman.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod("Taragaman", "DBM-Party-Classic", 9)
 local L		= mod:GetLocalizedStrings()
 mod.statTypes = "normal,heroic,mythic"
 
-mod:SetRevision("20220725002300KSM")
+mod:SetRevision("20220728142900")
 mod:SetCreatureID(11520)
 
 mod:RegisterCombat("combat")
